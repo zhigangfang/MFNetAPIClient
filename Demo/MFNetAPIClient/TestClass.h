839358b5ad74e479fc8b760c417244828068b9ff
房志刚 <Meigan@fangzhigangs-MacBook-Pro.local>
2017-03-21 11:30:42 +0800

    delete demo

---
+++ Demo/MFNetAPIClient/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  MFNetAPIClient
+//
+//  Created by 房志刚 on 21/3/2017.
+//  Copyright © 2017 bjdv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ Demo/MFNetAPIClient/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  MFNetAPIClient
+//
+//  Created by 房志刚 on 21/3/2017.
+//  Copyright © 2017 bjdv. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
